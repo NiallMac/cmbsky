fb7b8122cbee176748c10b4dc636f5fec5ecaa8a
NiallMac <niall.maccrann@gmail.com>
2022-07-12 08:10:00 -0700

    started on psh+prh

--- cmbsky/reconstruction.py
+++ cmbsky/reconstruction.py
@@ -70,8 +70,8 @@ def setup_recon(px, lmin, lmax, mlmax,
                 tcls_X, tcls_Y=None,
                 tcls_XY=None, do_pol=False,
                 do_Tpol=False,
-                do_psh=False, do_prh=False,
-                profile=None):
+                do_psh=False, do_prh=False, do_psh_prh=False,
+                profile=None, get_pol_norms=True):
     """
     Setup needed for reconstruction and foreground
     bias estimation. If cltot_Y and cltot_XY are not 
@@ -79,21 +79,23 @@ def setup_recon(px, lmin, lmax, mlmax,
     """
     ucls,_ = futils.get_theory_dicts(grad=True, lmax=mlmax)
 
-    if do_pol:
+    if do_pol or get_pol_norms:
         pols = ["TE", "TB", "EE", "EB", "BB"]
+        assert "EE" in tcls_X
+        assert "BB" in tcls_X
     elif do_Tpol:
         pols = ["TE", "TB"]
     else:
         pols = []
-    for pol in pols:
-        assert pol in tcls_X
         
     recon_stuff = {}
 
     norms_X = pytempura.get_norms(
-        ['TT']+pols, ucls, tcls_X,
-        lmin, lmax,
-	k_ellmax=mlmax)
+        ["TT"]+pols, ucls,
+        {c:tcls_X[c][:mlmax+1] for c in tcls_X.keys()},
+        lmin, lmax, k_ellmax=mlmax)
+    recon_stuff["norms"] = norms_X
+
     norm_lens_X = norms_X['TT']
 
     def filter_alms_X(alms):
@@ -214,7 +216,7 @@ def setup_recon(px, lmin, lmax, mlmax,
                 
             qfunc_tt_prh_X = solenspipe.get_qfunc(
                 px, ucls, mlmax, "TT",
-                Al1=norms_X['TT'], est2='PH', Al2=norm_prof,
+                Al1=norms_X['TT'], est2='SRC', Al2=norm_prof,
                 R12=R_prof_tt, profile=profile)
 
             recon_stuff["profile"] = profile
@@ -254,6 +256,95 @@ def setup_recon(px, lmin, lmax, mlmax,
             
             recon_stuff["get_fg_trispectrum_phi_N0_prh"] = get_fg_trispectrum_phi_N0_prh
 
+            if do_psh_prh:
+                try:
+                    assert (do_psh and do_prh)
+                except AssertionError as e:
+                    print("need do_psh=True and do_prh=True for do_psh_prh=True")
+                #Since we've set up profile-hardening, we might as well also set up
+                #source + profile-hardening. Should just need the profile-source response
+                #in addition to everything else we've computed already.
+                R_prof_src = (1./pytempura.get_norms(
+                    ['src'], ucls, tcls_X,
+                    lmin, lmax, k_ellmax=mlmax,
+                    profile = profile**0.5)['src'])
+                R_prof_src[0] = 0.
+                R_matrix = np.ones((mlmax, 3, 3))
+                R_matrix[:,0,1] = norm_lens_X[0] * R_src_tt
+                R_matrix[:,0,2] = norm_lens_X[0] * R_prof_tt
+                R_matrix[:,1,0] = norm_src * R_src_tt
+                R_matrix[:,1,2] = norm_src * R_prof_src
+                R_matrix[:,2,0] = norm_prof * R_prof_tt
+                R_matrix[:,2,1] = norm_prof * R_prof_src
+                R_inv = np.zeros_like(R_matrix)
+                for l in range(mlmax+1):
+                    R_inv[l] = np.linalg.inv(R_matrix[l])
+
+                def qfunc_psh_prh(X_filtered, Y_filtered):
+                    phi = qfunc_tt_X(X_filtered, Y_filtered)[0]
+                    ps = qfunc_tt_psh_X(X_filtered, T_filtered)
+                    pr = qfunc_tt_prh_X(X_filtered, T_filtered)
+                    #to get phi, we just need the first two of the
+                    #R_inv matrix
+                    phi_bh = (phi[0] + curvedsky.almxfl(R_inv[0,1], ps)
+                              + curvedsky.almxfl(R_inv[0,2], pr),
+                              phi[1]
+                              )
+                    return phi_bh
+
+                R_other = R_matrix[:, 1:, 1:]
+                detR = np.array([np.linalg.det(R_matrix[l]) for l in range(mlmax+1)])
+                detR_other = np.array([np.linalg.det(R_other[l]) for l in range(mlmax+1)])
+                print("R_inv_ps_pr.shape:", R_inv_ps_pr.shape)
+                recon_stuff["N0_phi_prh"] = (
+                    norm_lens_X[0] * detR_other / detR,
+                    norm_lens_X[1]
+                )
+                def get_fg_trispectrum_phi_N0_psh_prh(cl_fg):
+                    Ctot = tcls_X['TT']**2 / cl_fg
+                    norm_lens = norm_lens_X
+                    norm_fg = pytempura.norm_lens.qtt(
+                        mlmax, lmin,
+                        lmax, ucls['TT'],
+                        Ctot,gtype='')
+
+                    norm_ps_fg = pytempura.get_norms(
+                        ['TT','src'], ucls, {'TT':Ctot},
+                        lmin, lmax,
+                        k_ellmax=mlmax)['src']
+                    R_ps_fg = pytempura.get_cross(
+                        'SRC','TT', ucls, {'TT':Ctot},
+                        lmin, lmax,
+                        k_ellmax=mlmax)
+                    norm_prof_fg = pytempura.get_norms(
+                        ['TT','src'], ucls, {'TT':Ctot},
+                        lmin, lmax,
+                        k_ellmax=mlmax, profile=profile)['src']
+                    R_prof_fg = pytempura.get_cross(
+                        'SRC','TT', ucls, {'TT':Ctot},
+                        lmin, lmax,
+                        k_ellmax=mlmax, profile=profile)
+                    R_prof_ps_fg = (1./pytempura.get_norms(
+                        ['src'], ucls, {'TT':Ctot},
+                        lmin, lmax, k_ellmax=mlmax,
+                        profile = profile**0.5)['src'])
+
+                    #gradient and curl
+                    N0_tris=[]
+                    for i in [0,1]:
+                        N0 = norm_lens[i]**2 / norm_fg[i]
+                        N0_ps = norm_ps**2/norm_ps_fg
+                        N0_prof = norm_prof**2/norm_prof_fg
+                        #continue from here
+                        N0_tri = (N0 + R_prof_tt**2 * norm_lens[i]**2 * N0_prof
+                                  - 2 * R_prof_tt * norm_lens[i]**2 * norm_prof * R_src_fg)
+                        N0_tri /= (1 - norm_lens[i]*norm_prof*R_prof_tt**2)**2
+                        N0_tris.append(N0_tri)
+                    return tuple(N0_tris)
+
+                recon_stuff["get_fg_trispectrum_phi_N0_prh"] = get_fg_trispectrum_phi_N0_prh
+                
+
 
         if do_Tpol:
             qfunc_te_X = solenspipe.get_qfunc(px, ucls, mlmax, "TE", Al1=norms_X['TE'])
@@ -276,8 +367,8 @@ def setup_recon(px, lmin, lmax, mlmax,
                     'SRC','TE',ucls,tcls,lmin,lmax,
                     k_ellmax=mlmax, profile=profile)
                 qfunc_te_prh_X = solenspipe.get_qfunc(
-                    px, ucls, mlmax,"TE", est2='PH', Al1=norms_X['TE'],
-                    Al2=norm_prof['src'], R12=R_prof_te)
+                    px, ucls, mlmax,"TE", est2='SRC', Al1=norms_X['TE'],
+                    Al2=norm_prof['src'], R12=R_prof_te, profile=profile)
                 recon_stuff["qfunc_te_psh"] = qfunc_te_prh_X
                 recon_stuff["qfunc_te_psh_incfilter"] = lambda X,Y: qfunc_te_prh_X(
                     filter_alms_X(X), filter_alms(Y))
@@ -1757,12 +1848,12 @@ def plot_secondary_terms():
     fig.savefig(opj(outdir, "secondary_terms.png"), dpi=200) 
 """
 
-def get_websky_lensed_cmb(cmb_seed):
-    f = opj(WEBSKY_DIR, "lensed_cmb", "lensed_cmb_alm_websky_cmb%d_lmax6000.fits"%cmb_seed)
+def get_websky_lensed_cmb(cmb_seed, websky_dir=WEBSKY_DIR):
+    f = opj(websky_dir, "lensed_cmb", "lensed_cmb_alm_websky_cmb%d_lmax6000.fits"%cmb_seed)
     return hp.fitsfunc.read_alm(f, hdu=(1,2,3))
 
-def get_sehgal_lensed_cmb(cmb_seed):
-    f = opj(SEHGAL_DIR, "lensed_cmb", "lensed_cmb_alm_sehgal_cmb%d_lmax6000.fits"%cmb_seed)
+def get_sehgal_lensed_cmb(cmb_seed, sehgal_dir=SEHGAL_DIR):
+    f = opj(sehgal_dir, "lensed_cmb", "lensed_cmb_alm_sehgal_cmb%d_lmax6000.fits"%cmb_seed)
     return hp.fitsfunc.read_alm(f, hdu=(1,2,3))
     
 def test_secondary(use_mpi=False, nsim=10, from_pkl=False):
